ab2417c93f089bac69b006b7dfa93104a60aef84
Mikita <anykey2solo@gmail.com>
2023-10-30 21:25:47 +0300

    feat: create Header component

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,51 @@
 import { Component } from "react";
+import {
+  HeaderProps,
+  HeaderState,
+  PeopleResponse,
+  SearchResponse,
+} from "./apiTypes";
 
-export default class Header extends Component {
-  state = { inputValue: localStorage.getItem("search-query") };
+export default class Header extends Component<HeaderProps, HeaderState> {
+  state = {
+    inputValue: localStorage.getItem("search-query") || "",
+  };
+
+  inputChange = (event: React.FormEvent<HTMLInputElement>) => {
+    this.setState({ inputValue: event.currentTarget.value });
+  };
+
+  buttonClick = () => {
+    localStorage.setItem("search-query", this.state.inputValue);
+    this.getData();
+  };
+
+  async getData() {
+    const storageData: string = localStorage.getItem("search-query") || "";
+    const response: Response = await fetch(
+      `https://swapi.dev/api/people/?search=${storageData}`,
+    );
+    const searchResponse: SearchResponse = await response.json();
+    const data: PeopleResponse[] = searchResponse.results;
+    this.props.changeState(data);
+  }
+
+  componentDidMount(): void {
+    this.getData();
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          <input
+            type="text"
+            value={this.state.inputValue}
+            onChange={this.inputChange}
+          />
+          <button onClick={this.buttonClick}>Search</button>
+        </div>
+      </>
+    );
+  }
 }
