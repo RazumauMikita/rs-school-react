545eab7483c0971259b21133a23254ac324b72c7
Mikita <anykey2solo@gmail.com>
2023-10-30 21:24:54 +0300

    feat: create apiType file

---
+++ src/apiTypes.ts
@@ -0,0 +1,49 @@
+type PeopleResponse = {
+  name: string;
+  birth_year: string;
+  eye_color: string;
+  gender: string;
+  hair_color: string;
+  height: string;
+  mass: string;
+  skin_color: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  starships: string[];
+  vehicles: string[];
+  url: string;
+  created: string;
+  edited: string;
+};
+
+type SearchResponse = {
+  count: number;
+  next: null;
+  previous: null;
+  results: PeopleResponse[];
+};
+
+interface DataViewProps {
+  data: PeopleResponse[];
+}
+
+interface HeaderState {
+  inputValue: string;
+}
+
+interface HeaderProps {
+  changeState: (newData: PeopleResponse[]) => void;
+}
+
+interface AppState {
+  people: PeopleResponse[];
+}
+export type {
+  PeopleResponse,
+  SearchResponse,
+  DataViewProps,
+  HeaderState,
+  HeaderProps,
+  AppState,
+};
