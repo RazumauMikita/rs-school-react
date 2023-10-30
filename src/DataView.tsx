84bffbddcf22a495c74def546d447e4427e4eb9d
Mikita <anykey2solo@gmail.com>
2023-10-30 21:27:25 +0300

    refactor: remove redundant part of component

--- src/DataView.tsx
+++ src/DataView.tsx
@@ -1,26 +1,5 @@
 import { Component } from "react";
-type PeopleResponse = {
-  name: string;
-  birth_year: string;
-  eye_color: string;
-  gender: string;
-  hair_color: string;
-  height: string;
-  mass: string;
-  skin_color: string;
-  homeworld: string;
-  films: string[];
-  species: string[];
-  starships: string[];
-  vehicles: string[];
-  url: string;
-  created: string;
-  edited: string;
-};
-
-interface DataViewProps {
-  data: PeopleResponse[];
-}
+import { DataViewProps } from "./apiTypes";
 
 export default class DataView extends Component<DataViewProps> {
   constructor(props: DataViewProps) {
@@ -29,14 +8,16 @@ export default class DataView extends Component<DataViewProps> {
   render() {
     return (
       <div className="data-container">
-        {this.props.data.map((el, index) => {
-          return (
-            <div className="item__container" key={index}>
-              <span>Name: {el.name}</span>
-              <span>Birth year: {el.birth_year}</span>
-            </div>
-          );
-        })}
+        {this.props.data.length
+          ? this.props.data.map((el, index) => {
+              return (
+                <div className="item__container" key={index}>
+                  <span>Name: {el.name}</span>
+                  <span>Birth year: {el.birth_year}</span>
+                </div>
+              );
+            })
+          : "load"}
       </div>
     );
   }
