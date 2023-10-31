949e422697539b0cd345b4b2f59cdc1bbd1c18da
Mikita <anykey2solo@gmail.com>
2023-10-31 23:00:44 +0300

    feat: add ErrorBoundary to main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import ErrorBoundary from "./ErrorBoundary.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallback={<p>Oops! Looks like an error!</p>}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 );
