539ea6c79c372279449110bd0e6f3026b30d8a01
Mikita <anykey2solo@gmail.com>
2023-11-04 12:06:50 +0300

    refactor: change dataviewer to functional component

--- src/Components/DataViewer/DataViewer.tsx
+++ src/Components/DataViewer/DataViewer.tsx
@@ -1,33 +1,32 @@
-import { Component } from "react";
+import { FC } from "react";
 import { DataViewerProps } from "./DataViewer.type";
 import styles from "./DataViewer.module.css";
-export default class DataViewer extends Component<
-  DataViewerProps,
-  Record<never, string>
-> {
-  render() {
-    return (
-      <div className={styles.data_container}>
-        {this.props.loadStatus ? (
-          this.props.data.map((el, index) => {
-            return (
-              <div className={styles.item} key={index}>
-                <span>
-                  <strong>Name:</strong> {el.name}
-                </span>
-                <span>
-                  <strong>Birth year:</strong> {el.birth_year}
-                </span>
-                <span>
-                  <strong>Eye color:</strong> {el.eye_color}
-                </span>
-              </div>
-            );
-          })
-        ) : (
-          <span className="">loading...</span>
-        )}
-      </div>
-    );
-  }
-}
+
+const DataViewerF: FC<DataViewerProps> = (props) => {
+  const { data, loadStatus } = { ...props };
+  return (
+    <div className={styles.data_container}>
+      {loadStatus ? (
+        data.map((el, index) => {
+          return (
+            <div className={styles.item} key={index}>
+              <span>
+                <strong>Name:</strong> {el.name}
+              </span>
+              <span>
+                <strong>Birth year:</strong> {el.birth_year}
+              </span>
+              <span>
+                <strong>Eye color:</strong> {el.eye_color}
+              </span>
+            </div>
+          );
+        })
+      ) : (
+        <span className="">loading...</span>
+      )}
+    </div>
+  );
+};
+
+export default DataViewerF;
