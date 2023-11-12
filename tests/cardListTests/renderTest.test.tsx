979aa93fca878f1582c3ea561bddadaf48819405
Mikita <anykey2solo@gmail.com>
2023-11-12 21:55:06 +0300

    feat: add pre push test

--- tests/cardListTests/renderTest.test.tsx
+++ tests/cardListTests/renderTest.test.tsx
@@ -33,7 +33,7 @@ describe("test Card List", () => {
   test("DataView component renders the specified number of cards", async () => {
     useContextMock.mockReturnValue(defaultValueFill);
     const element = new ShallowRenderer().render(
-      <DataViewerF loadStatus={false} page={"1"} setToggleSide={jest.fn()} />
+      <DataViewerF loadStatus={false} page={"1"} setToggleSide={jest.fn()} />,
     );
     expect(element.props.children).toHaveLength(SPECIFIED_NUMBERS_OF_CARDS);
   });
@@ -41,7 +41,7 @@ describe("test Card List", () => {
   test("An appropriate message is displayed if no cards are present", () => {
     useContextMock.mockReturnValue(defaultValueVoid);
     const element = new ShallowRenderer().render(
-      <DataViewerF loadStatus={false} page={"1"} setToggleSide={jest.fn()} />
+      <DataViewerF loadStatus={false} page={"1"} setToggleSide={jest.fn()} />,
     );
 
     expect(element.props.children).toEqual([
